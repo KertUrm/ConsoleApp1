c6758b553d566a166a9003b1532514d25c3ed2a8
Opilane TTHK <opilane@tthk.ee>
2023-02-13 12:59:09 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] n = {"Anton Tammsaare","Lydia Koidula","August Gail","Betti Alver" };
+            foreach (string s in n)
+                Console.WriteLine(s);
+        }
+    }
+}
